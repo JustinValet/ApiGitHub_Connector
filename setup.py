3b4abf32e80474669623c06438abc4daf9e4a4b6
JustinValet <jv.datamail@gmail.com>
2019-06-23 12:35:01 +0200

    this is the first commit

--- setup.py
+++ setup.py
@@ -3,13 +3,19 @@ from setuptools import setup, find_packages
 with open("requierements.txt") as f:
     requierements = f.read()
 
+classifiers = [
+    'Programming Language :: Python :: 3.6'
+]
+
 setup(
   name='ApiGitHub_Connector',
-  version = '0.0.1',
-  requierements= requierements,
-  packages=find_packages(),
-  url='',
-  license='',
+  description='This packages is a wrapper for the GitHub API',
   author='Justin Valet',
-  description='This packages is a wrapper for the GitHub API'
+  author_email='jv.datamail@gmail.com',
+  version= '0.0.1',
+  classifiers=classifiers,
+  requierements=requierements,
+  packages=find_packages(),
+  url='https://github.com/JustinValet/ApiGitHub_Connector',
+  license=''
 )
